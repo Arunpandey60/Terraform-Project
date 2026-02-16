af373a559c9577af60a39c1f0373687ac5851730
arun.pandey <arun.pandey@mpahsis.com>
2026-02-16 22:21:58 +0530

    terraform v0.1

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
-provider "aws" {
-  region = var.region
-}
-
 module "vpc" {
   source = "./modules/vpc"
   vpc_cidr = var.vpc_cidr
   azs      = var.azs
 }
 
-module "security" {
+module "security-group" {
   source = "./modules/security-group"
   vpc_id = module.vpc.vpc_id
 }
@@ -17,14 +13,14 @@ module "alb" {
   source = "./modules/alb"
   vpc_id         = module.vpc.vpc_id
   public_subnets = module.vpc.public_subnets
-  sg_alb         = module.security.sg_alb
+  sg_alb         = module.security-group.sg_alb
   certificate_arn = var.certificate_arn
 }
 
 module "asg" {
   source           = "./modules/asg"
   private_subnets  = module.vpc.private_subnets
-  sg_ec2           = module.security.sg_ec2
+  sg_ec2           = module.security-group.sg_ec2
   target_group_arn = module.alb.target_group_arn
   ami_id           = var.ami_id
   instance_type    = var.instance_type
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+region = "value"
+vpc_cidr = "value"
+azs = ["value1", "value2", "value3"]
+certificate_arn = "value"
+ami_id = "value"
+instance_type = "value"
