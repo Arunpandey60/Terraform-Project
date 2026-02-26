d04c3428e083a3f78ad5bce44efdf5a2575f0d62
Arunpandey60 <arunpanedu@gmail.com>
2026-02-26 15:22:46 +0530

    Version 1.0

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -1,5 +1,6 @@
 resource "aws_iam_role" "ec2_role" {
   name = "ec2-app-role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -18,7 +19,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 }
 
 resource "aws_launch_template" "app" {
-  name_prefix   = "app-template"
+  name_prefix   = "CitiusCloud-LaunchTemp"
   image_id      = var.ami_id
   instance_type = var.instance_type
 
@@ -33,7 +34,7 @@ resource "aws_launch_template" "app" {
     ebs {
       volume_size = 20
       volume_type = "gp3"
-      encrypted   = true 
+      encrypted   = true
     }
   }
 
@@ -41,8 +42,43 @@ resource "aws_launch_template" "app" {
               #!/bin/bash
               apt-get update -y
               apt-get install -y nginx docker.io
+              systemctl enable nginx
               systemctl start nginx
               echo "<h1>Hello from Terraform ASG</h1>" > /var/www/html/index.html
               EOF
   )
+}
+
+resource "aws_autoscaling_group" "app_asg" {
+  name                = "app-asg"
+  desired_capacity    = 1
+  min_size            = 1
+  max_size            = 2
+
+  vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-instance"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_policy" "cpu_target" {
+  name                   = "cpu-target-tracking"
+  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 60.0
+  }
 }
